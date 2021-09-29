04d76ba3be3cc9bb4ff507fe22061b006c9dc5fa
Anil <64589057+sononeanil@users.noreply.github.com>
2021-09-29 16:44:58 +0530

    initial commit

---
+++ src/main/java/com/xoriant/vaccination/VaccinationMaintainerApplication.java
@@ -0,0 +1,13 @@
+package com.xoriant.vaccination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationMaintainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationMaintainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xoriant/vaccination/VaccinationMaintainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xoriant.vaccination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationMaintainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
